f87d780387c2c1b10fd52f0d2775596e8d97a3e7
Tsion Tesfaye <tsionregassa042@gmail.com>
2025-06-25 16:30:02 +0800

    Add new components and updates

---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+function Header(){
+return(
+<h1>food website</h1>
+);
+
+}
+export default Header
---
+++ src/Navbar.jsx
@@ -0,0 +1,12 @@
+function Navbar(){
+return(
+<ul>
+<li>Home</li>
+<li>about us</li>
+<li>contact</li>
+
+</ul>
+);
+
+}
+export default Navbar
